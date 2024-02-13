d2dd4bac907dc082a2a2a75d2c01d4b0e31b5485
222hojun <sciboviator@gmail.com>
2024-02-13 18:02:27 +0900

    for and test

---
+++ src/main/java/com/ohgiraffers/section02/looping/Application01.java
@@ -0,0 +1,41 @@
+package com.ohgiraffers.section02.looping;
+
+import com.ohgiraffers.section01.conditional.A_if;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+        A_for a = new A_for();
+
+//        a.testSimpleForStatement();
+
+//        a.testForExample01();
+
+//        a.testForExatmple02();
+
+//        a.testForExample03();
+
+//        a.testForExample04();
+
+//        a.printSimpleGugudan();
+
+        A_nestedFor b = new A_nestedFor();
+
+//        b.printGugudanFromTwoToNine();
+
+//        b.printUpgradeGugudan();
+
+//        b.printStatInputRowTimes();
+
+        b.pritnStarInputRowTimes02();
+
+
+
+
+
+
+
+    }
+
+}
---
+++ src/main/java/com/ohgiraffers/section02/test/Application01.java
@@ -0,0 +1,26 @@
+package com.ohgiraffers.section02.test;
+
+public class Application01 {
+
+    public static void main(String[] args) {
+
+
+        ForStatement f = new ForStatement();
+
+//        f.countNormal();
+
+//        f.countReverse();
+
+//        f.Gugudan7();
+
+//        f.evenNum();
+
+//        f.factorial();
+
+//        f.star02();
+
+//        f.star03();
+    }
+
+
+}
